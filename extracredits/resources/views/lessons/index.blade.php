6b3d19abc8f01650d9670abf57180ae6a66268da
Ralph <info@collage.ie>
2020-07-13 17:50:03 +0100

    card view of lessons

--- extracredits/resources/views/lessons/index.blade.php
+++ extracredits/resources/views/lessons/index.blade.php
@@ -1,5 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="col">
     <h2>List of lessons go here</h2>
+    <div class="row">
+        @foreach ($lessons as $lesson)
+        <div class="col-4">
+            <div class="card">
+                <img src="/images/thumbnails/{{ $lesson->thumbnail }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $lesson->title }}</h5>
+                <p class="card-text">{{ Str::words($lesson->description, 12, '  ...') }}</p>
+                    <a href="#" class="btn btn-primary">View</a>
+                </div>
+            </div>
+        </div>
+        @endforeach
+
+    </div>
+    <ul>
+    </ul>
+</div>
 @endsection
