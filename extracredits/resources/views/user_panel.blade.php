16663fbc48bd70af6dd092578f597426a652f774
redlik <ralphr@outlook.com>
2020-08-17 17:49:14 +0100

    user panel - removed dashboard link

--- extracredits/resources/views/user_panel.blade.php
+++ extracredits/resources/views/user_panel.blade.php
@@ -2,8 +2,7 @@
 
 @section('content')
 <div class="col">
-    <a class="btn btn-primary mb-4" href="{{ route('dashboard') }}/#users"> << Back to dashboard</a>
-    <h3 class="mb-4">User name: <strong>{{ $user->name }}</strong></h3>
+    <h3 class="mb-4">User name: <strong>{{ Auth::user()->name }}</strong></h3>
     <h4>Number of credits: <strong>{{ $user->credits }}</strong></h4>
     <a href="{{ route('buy_credits') }}" class="btn btn-success">Buy more credits</a>
     <hr>
