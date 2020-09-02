aeaa22116e36c1e9c3af3af09c73243178f0be40
redlik <ralphr@outlook.com>
2020-09-02 17:52:16 +0100

    Stripe checkout step 1

--- extracredits/config/app.php
+++ extracredits/config/app.php
@@ -229,6 +229,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Debugbar' => Barryvdh\Debugbar\Facade::class,
+        'Stripe' => Cartalyst\Stripe\Laravel\Facades\Stripe::class,
 
     ],
 
