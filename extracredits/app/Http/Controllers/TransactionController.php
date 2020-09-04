34ae5679b30266fb92c0695b313c141d69ede7a3
redlik <ralphr@outlook.com>
2020-09-04 15:10:39 +0100

    Success message added to user profile

--- extracredits/app/Http/Controllers/TransactionController.php
+++ extracredits/app/Http/Controllers/TransactionController.php
@@ -12,7 +12,6 @@ class TransactionController extends Controller
 {
     public function store(Request $request) {
         $credit = (int)$request->get('credit_topup');
-        $user_id = Auth::id();
         $transaction = new Transaction($request->all());
         switch ($credit) {
             case 5:
@@ -32,13 +31,13 @@ class TransactionController extends Controller
         }
         
         try {
-            $charge = Stripe::charges()->create([
-                'amount' => $amount,
-                'currency' => 'EUR',
-                'source' => $request->stripeToken,
-                'description' => 'Exams Made Easy',
-                'receipt_email' => $request->email,
-            ]);
+            // $charge = Stripe::charges()->create([
+            //     'amount' => $amount,
+            //     'currency' => 'EUR',
+            //     'source' => $request->stripeToken,
+            //     'description' => 'Exams Made Easy',
+            //     'receipt_email' => $request->email,
+            // ]);
                 
                 // Adding purchased credits to user account
                 $user = Auth::user();
@@ -47,8 +46,12 @@ class TransactionController extends Controller
                 $user->save();
 
                 //Saving transaction details
+                $transaction->user_id = Auth::id();
                 $transaction->amount = $amount;
                 $transaction->save();
+
+                // session(['success_message' => 'Thank you for the purchase, the credits has been added to your account']);
+                $request->session()->flash('success_message', 'Thank you for the purchase, the credits has been added to your account');
 
                 $lessons = Lesson::withCount('user')->get();
                 return view('user_panel', ['user' => $user, 'lessons' => $lessons]);
