6eb990352fc21feff1d13d353063443afca455f9
redlik <ralphr@outlook.com>
2020-10-08 17:46:53 +0100

    livewire component in progress

--- extracredits/app/Http/Livewire/Lessons.php
+++ extracredits/app/Http/Livewire/Lessons.php
@@ -2,18 +2,31 @@
 
 namespace App\Http\Livewire;
 
+use App\User;
 use Livewire\Component;
 use App\Lesson;
+use App\Subject;
+use Auth;
 
 class Lessons extends Component
 {
     public $search;
 
-    protected $queryString = ['search'];
+    public $subject;
 
+    protected $queryString = ['search', 'subject'];
+
+    protected $query;
 
     public function render()
     {
-        return view('livewire.lessons', ['lessons' => Lesson::where('title', 'like', '%'.$this->search.'%')->get()]);
+        $subject = $this->subject;
+        $query = Lesson::where('title', 'like', '%'.$this->search.'%')->when($subject, function ($query, $subject){
+            return $query->where('subject_id', $subject);
+        })->has('user', '=', Auth::id())
+        ->get();
+
+        $subjects = Subject::orderBy('name', 'asc')->get();
+        return view('livewire.lessons', ['lessons' => $query, 'subjects'=>$subjects]);
     }
 }
--- extracredits/resources/views/livewire/lessons.blade.php
+++ extracredits/resources/views/livewire/lessons.blade.php
@@ -1,5 +1,17 @@
-<div>
-    <input wire:model="search" type="search" placeholder="Search lessons by title...">
+<div class="d-block w-100 p-2">
+    <div class="d-flex justify-content-between">
+        <div class="form-group">
+            <select wire:model="subject" name="subjects" id="" class="form-control">
+                <option value="" selected>All subjects</option>
+                @foreach($subjects as $subject)
+                    <option value="{{ $subject->id }}">{{ ucfirst($subject->name) }}</option>
+                @endforeach
+            </select>
+        </div>
+        <div class="form-group">
+            <input class="form-control" wire:model="search" type="search" name="search" placeholder="Search lessons by title...">
+        </div>
+    </div>
     <ul>
         @foreach ($lessons as $lesson)
             <li>{{ $lesson->title }}
