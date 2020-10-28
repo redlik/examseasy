6759ccc664d6a9ab1282a82ec390141e67b7fd54
redlik <ralphr@outlook.com>
2020-10-28 21:29:55 +0000

    credit cost hidden

--- extracredits/resources/views/lessons/subject-category-topic.blade.php
+++ extracredits/resources/views/lessons/subject-category-topic.blade.php
@@ -26,11 +26,8 @@
                                             <div class="mb-auto p-sm-2">
                                                 <h5 class="display-6 font-weight-bold">{{ $lesson->title }}</h5>
                                                 <p class="card-text">{{ Str::words($lesson->description, 20, '  ...') }}<br/>
-                                                    @auth
-                                                        @if (Auth::user()->unlimited == 0)
+                                                   
                                                         <span class="font-weight-bold">Credit cost: {{ $lesson->credit_cost }}</span></p>
-                                                        @endif
-                                                    @endauth
                                             </div>
                                            <div class="p-sm-2">
                                                @role('student')
