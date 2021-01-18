c5af155d9c0a7fc475296c936881dfb030d5e45a
redlik <ralphr@outlook.com>
2021-01-18 20:32:19 +0000

    Added script to prevent multiple clicks on lesson unlock

--- extracredits/resources/views/lessons/subject-category-topic.blade.php
+++ extracredits/resources/views/lessons/subject-category-topic.blade.php
@@ -26,7 +26,7 @@
                                             <div class="mb-auto p-sm-2">
                                                 <h5 class="display-6 font-weight-bold">{{ $lesson->title }}</h5>
                                                 <p class="card-text">{{ Str::words($lesson->description, 20, '  ...') }}<br/>
-                                                   
+
                                                         <span class="font-weight-bold">Credit cost: {{ $lesson->credit_cost }}</span></p>
                                             </div>
                                            <div class="p-sm-2">
@@ -35,7 +35,7 @@
                                                        <p><small class="text-danger mb-3">You don't have enough credits to unlock this lesson</small></p>
                                                    @endif
                                                @endrole
-       
+
                                                @guest
                                                    <a href="{{ route('login') }}" class="btn btn-warning">Login/Register to view</a>
                                                @endguest
@@ -48,7 +48,7 @@
                                                    @else
                                                    @if (Auth::user()->unlimited == 1)
                                                         <a href="{{ route('lesson_canonical_view', [$lesson->subject->name, $lesson->topic->subcategory->slug, $lesson->topic->slug, $lesson->slug]) }}" class="btn btn-success">View <i
-                                                            class="fas fa-tv ml-2"></i></a>  
+                                                            class="fas fa-tv ml-2"></i></a>
                                                             @else
                                                                 @if (user_unlocked($lesson->id) )
                                                                     <a href="{{ route('lesson_canonical_view', [$lesson->subject->name, $lesson->topic->subcategory->slug, $lesson->topic->slug, $lesson->slug]) }}" class="btn btn-success">View <i
@@ -57,14 +57,21 @@
                                                                     <a href="{{ route('buy_credits') }}" class="btn btn-danger"><i class="fas fa-cart-plus mr-2"></i>
                                                                         Buy more credits</a>
                                                                 @else
-                                                                    <a href="{{ route('is-unlocked', [$lesson->id]) }}" class="btn btn-warning"><i
-                                                                            class="fas fa-unlock mr-2"></i> Unlock for {{ $lesson->credit_cost }} 
+                                                                    <a href="{{ route('is-unlocked', [$lesson->id])
+                                                                    }}" class="btn btn-warning" id="unlock-button"><i
+                                                                            class="fas fa-unlock mr-2"></i> Unlock for {{ $lesson->credit_cost }}
                                                                             @if ($lesson->credit_cost == 1)
                                                                                 credit
                                                                             @else
                                                                                 credits
                                                                             @endif
                                                                             </a>
+                                                                    <div class="d-none"
+                                                                         id="unlock-message"><small>Unlocking the
+                                                                            lesson,
+                                                                            just a
+                                                                            moment...
+                                                                        </small></div>
                                                                 @endif
                                                     @endif
                                                    @endrole
@@ -76,9 +83,19 @@
                             </div>
                 @endforeach
         </div>
-            
-    
+
+
     </div>
-    
+
 </div>
+@endsection
+
+@section('bottom_scripts')
+    <script>
+        $( "#unlock-button" ).click(function(ev) {
+            console.log('You clicked!');
+            $("#unlock-button").addClass('d-none');
+            $('#unlock-message').removeClass('d-none');
+        });
+    </script>
 @endsection
