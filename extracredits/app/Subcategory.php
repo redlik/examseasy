01a5dd766981b7396bda180b1b79759290889467
redlik <ralphr@outlook.com>
2021-01-29 11:45:21 +0000

    Added relationship to category to count number of lessons

--- extracredits/app/Subcategory.php
+++ extracredits/app/Subcategory.php
@@ -7,12 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class Subcategory extends Model
 {
     protected $fillable = array('name', 'slug');
-    
+
     public function subject() {
         return $this->belongsTo('App\Subject', 'subject_id');
     }
-    
+
     public function topic() {
         return $this->hasMany('App\Topic');
+    }
+
+    public function lessons()
+    {
+        return $this->hasManyThrough(Lesson::class, Topic::class);
     }
 }
